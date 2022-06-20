cc7fa768cbd7f1306788bd976bebd1697473af50
John Soares <johnsued25@gmail.com>
2022-06-20 09:32:32 -0300

    finaldeploy

--- treino-js/Joao.js
+++
@@ -1,7 +0,0 @@
-var produtos = document.querySelectorALL(".produtos");
-
-for( var i = 0 ; i < produtos.length ; i++){
-    var produto  = produtos[i];
-    var nomeDoProduto = produto.textContent;
-    console.log(nomeDoProduto);
-}
